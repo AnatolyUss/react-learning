57b3384e4fc8b0796eae3531f00ce995ad18133a
Anatoly <anatolyuss@gmail.com>
2018-11-20 06:37:09 +0200

    added houses search

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './main-page.css';
 import Header from './header';
 import FeaturedHouse from './featured-house';
+import HouseFilter from './house-filter';
+import SearchResults from '../search-results';
+import HouseDetail from '../house';
 
 class App extends Component {
   constructor (props) {
@@ -16,14 +19,15 @@ class App extends Component {
     this.fetchHouses();
   }
 
-  async fetchHouses() {
+  fetchHouses = async () => {
     const response = await fetch('./houses.json');
     const allHouses = await response.json();
     this.allHouses = allHouses;
     this.determineFeaturedHouses();
+    this.determineUniqueCountries();
   }
 
-  determineFeaturedHouses() {
+  determineFeaturedHouses = () => {
     if (this.allHouses) {
       const randomIndex = Math.floor(Math.random() * this.allHouses.length);
       const featuredHouse = this.allHouses[randomIndex];
@@ -31,11 +35,42 @@ class App extends Component {
     }
   }
 
+  determineUniqueCountries = () => {
+    const countries = this.allHouses ? Array.from(new Set(this.allHouses.map(h => h.country))) : [];
+    countries.unshift(null); // Makes the first choice blank.
+    this.setState({ countries: countries });
+  }
+
+  setActiveHouse = (house) => {
+    this.setState({ activeHouse: house });
+  }
+
+  filterHouses = (country) => {
+    this.setState({ activeHouse: null });
+    const filteredHouses = this.allHouses.filter(h => h.country === country);
+    this.setState({ filteredHouses: filteredHouses });
+    this.setState({ country: country });
+  }
+
   render() {
+    let activeComponent = null;
+    if (this.state.country) {
+      activeComponent = <SearchResults country={this.state.country} filteredHouses={this.state.filteredHouses} setActiveHouse={this.setActiveHouse} />;
+    }
+
+    if (this.state.activeHouse) {
+      activeComponent = <HouseDetail house={this.state.activeHouse}/>;
+    }
+
+    if (!activeComponent) {
+      activeComponent = <FeaturedHouse house={this.state.featuredHouse} />;
+    }
+
     return (
       <div className="container">
-        <Header subtitle="Providing houses worldwide."/>
-        <FeaturedHouse house={this.state.featuredHouse}/>
+        <Header subtitle="Providing houses all over the world"/>
+        <HouseFilter countries={this.state.countries} filterHouses={this.filterHouses} />
+        {activeComponent}
       </div>
     );
   }
